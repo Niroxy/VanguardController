c8582fc4a015ab4a06b87ed08e850922076212d5
Niroxy <saidhabilarvas@tutamail.com>
2024-06-05 00:35:26 +0300

    Add files via upload

---
+++ Launch-LoL.bat
@@ -0,0 +1,56 @@
+@echo off
+
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------    
+@ECHO OFF
+
+TASKKILL  /f /im RiotClientServices.exe
+kernelloader -l vgk.sys vgk
+sc.exe create vgc binPath= "C:\Program Files\Riot Vanguard\vgc.exe"
+sc config vgc start= demand
+net start vgc
+START "" "C:\Program Files\Riot Vanguard\VGTRAY.EXE"
+START "" "<RIOT CLIENT PATH>" --launch-product=league_of_legends --launch-patchline=live
+
+:LOOP
+tasklist | find /i "RiotClientServices" >nul 2>&1
+IF ERRORLEVEL 1 (
+  GOTO CONTINUE
+) ELSE (
+  ECHO LoL is still running
+  Timeout /T 5 /Nobreak
+  GOTO LOOP
+)
+
+:CONTINUE
+kernelloader -u vgk
+net stop vgc
+taskkill /f /im vgtray.exe
+sc config vgc start= disabled
+sc delete vgc
---
+++ ReadMe.txt
@@ -0,0 +1,9 @@
+(TR)
+Bu dosyaların hepsini "C:\Program Files\Riot Vanguard" dizinine atın. Sonra batch'lerin içindeki "<RIOT CLIENT PATH>" bölümüne "RiotClientServices.exe" dosyasının konumunu yazın. Sonra da batch dosyaları için masaüstüne kısayol oluşturun ve oradan açın. Batch'leri yönetici olarak açmayın kendisi yönetici izni isteyecek zaten. Oyunu kapatınca kernel driver dahil bütün vanguardı siliyor bildiğin. Hem bilgisayarınız hem gizliliğiniz rahat edecek.
+
+-Niroxy tarafından yapılmıştır.
+-----------------------------------
+(ENG)
+Copy all the files to "C:\Program Files\Riot Vanguard" path. Then edit the "<RIOT CLIENT PATH>" section of the batch files and type your "RiotClientServices.exe"s file path instead of it. After that, you can create a shortcut of the batch files to the desktop. Do not run the batch files with administrator privileges, the batch file itself is going to request it from you. When you close the game, it deletes (unloads) vanguard including the kernel driver. It's good for your PC and your privacy.
+
+-Made by Niroxy.
